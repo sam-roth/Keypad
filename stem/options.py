2b74327c916fdfd59daab409fd8939d371f73f03
Sam Roth <saroth@wisc.edu>
2014-02-05 20:21:56 -0600

    Fix issue #7. Use sensible font defaults that avoid the issue.

--- stem/options.py
+++ stem/options.py
@@ -1,12 +1,32 @@
-
+import os
 import os.path
 import pathlib
+import platform
+
 from .core import colorscheme
+
+
+OnPosixSystem       = os.name == 'posix'
+OnOSX               = platform.system() == 'Darwin'
+OnWindows           = platform.system() == 'Windows'
 
 UserConfigHome      = pathlib.Path(os.path.expanduser('~/.stem'))
 DefaultColorScheme  = colorscheme.SolarizedDark()
 DefaultDriverMod    = 'stem.qt.driver'
 
 
-TextViewFont                    = 'Menlo', 12
+
+
+
+DefaultOtherFont                = 'Monospace', 10
+DefaultOSXFont                  = 'Menlo', 12
+DefaultWinFont                  = 'Consolas', 10
+
+if OnOSX:
+    TextViewFont                = DefaultOSXFont
+elif OnWindows:
+    TextViewFont                = DefaultWinFont
+else:
+    TextViewFont                = DefaultOtherFont
+
 TextViewIntegerMetrics          = False
