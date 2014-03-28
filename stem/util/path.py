d8472a1a094d27de22c4aa14a8ae6db883b8db49
Sam Roth <saroth@wisc.edu>
2014-03-28 13:00:56 -0500

    Fix :e command for files that don't exist.
    --HG--
    branch : codemodel

--- stem/util/path.py
+++ stem/util/path.py
@@ -19,6 +19,15 @@ def search_upwards(path, glob):
 
         
 def same_file(path1, path2):
-    path1 = pathlib.Path(path1)
-    path2 = pathlib.Path(path2)
-    return os.path.samefile(str(path1.resolve()), str(path2.resolve()))
+    if path1 is None or path2 is None:
+        res = path1 is path2
+    else:    
+        try:
+            path1 = pathlib.Path(path1)
+            path2 = pathlib.Path(path2)
+            res = os.path.samefile(str(path1.resolve()), str(path2.resolve()))
+        except FileNotFoundError:
+            res = os.path.normpath(str(path1.absolute())) == os.path.normpath(str(path2.absolute()))
+    return res
+    
+    
