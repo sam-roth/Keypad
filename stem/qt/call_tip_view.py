9018e6955f0ebc9b8c96a9af5bad999292721a8b
Sam Roth <sam.roth1@gmail.com>
2014-06-05 14:10:24 -0500

    Remove commented out code in call_tip_view.py
    --HG--
    branch : new-text-layout

--- stem/qt/call_tip_view.py
+++ stem/qt/call_tip_view.py
@@ -33,14 +33,6 @@ class CallTipView(BasicTextView):
         
         
     def __reload_conf(self, *args):
-#         s = self.ct_settings
-#         if s.view_opacity < 0.95:
-#             self.setAttribute(Qt.Qt.WA_TranslucentBackground, True)
-#         else:
-#             self.setAttribute(Qt.Qt.WA_TranslucentBackground, False)
-            
-#             self.setWindowFlags(self.windowFlags() | Qt
-
         self.setWindowOpacity(self.ct_settings.view_opacity)
         
     @property
@@ -76,34 +68,4 @@ class CallTipView(BasicTextView):
 class MockCallTip(AbstractCallTip):    
     def to_astring(self, arg=None):
         return AttributedString('foo(bar, baz)')
-# 
-# show_mocktip = None
-# 
-# @interactive('add_mocktip')
-# def add_mocktip(_: object):
-#     global show_mocktip
-#     if show_mocktip is not None:
-#         return
-#     
-#     from ..control import BufferController
-#     
-#     @interactive('mocktip')
-#     def show_mocktip(bc: BufferController):
-#         show_mocktip_impl(bc.view)
-#     
-#     
-# def show_mocktip_impl(tv):
-#     t = CallTipView(tv.settings, tv)
-#     t.setFocusProxy(tv)
-#     t.move(tv.rect().center())
-#     t.model = MockCallTip()
-#     t.show()
-# 
-# 
-# @interactive('domt')
-# def do_mocktip(_: object):
-#     interactive.run('add_mocktip')
-#     interactive.run('mocktip')
-# 
-# @interactive('mt2')
-# def mt2(
+
