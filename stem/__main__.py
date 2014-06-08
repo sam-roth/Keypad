d463abaaa0769316cfbee111bedbc410cc90e837
Sam Roth <sam.roth1@gmail.com>
2014-06-08 11:43:13 -0500

    Allow for passing a "-c" flag to execute a command immediately on startup.
    --HG--
    branch : new-text-layout

--- stem/__main__.py
+++ stem/__main__.py
@@ -13,8 +13,36 @@ def main():
     import sys
     sys.path.insert(0, thirdparty)
     from . import config, options
-    import importlib
-    importlib.import_module(options.DefaultDriverMod).main()
+#     import importlib
+#     importlib.import_module(options.DefaultDriverMod).main()
+    from .qt import driver
+
+    # allow for sending a commandline command at startup
+    if len(sys.argv) > 2 and sys.argv[1] == '-c':
+        from .core.notification_queue import run_in_main_thread
+        from .control.command_line_interpreter import CommandLineInterpreter
+        from .abstract.application import app, AbstractApplication
+        from .core.errors import NoSuchCommandError
+
+        interp = CommandLineInterpreter()
+
+        def win_creation_hook(win):
+            # responder chain may not be updated yet
+            try:
+                interp.exec(app(), sys.argv[2])
+            except NoSuchCommandError:
+                interp.exec(win, sys.argv[2])
+            finally:
+                app().window_created.disconnect(win_creation_hook)
+
+        def app_creation_hook(_):
+            app().window_created.connect(win_creation_hook)
+
+
+        AbstractApplication.application_created.connect(app_creation_hook)
+
+
+    driver.main()
 
 if __name__ == '__main__':
     main()
