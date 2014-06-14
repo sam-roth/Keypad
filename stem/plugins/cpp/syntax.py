67b34dd1f553140729e6b8abc9e1f908ef2109ee
Sam Roth <sam.roth1@gmail.com>
2014-06-14 12:09:47 -0500

    Improve C++ syntax highlighting.

--- stem/plugins/cpp/syntax.py
+++ stem/plugins/cpp/syntax.py
@@ -11,19 +11,24 @@ def cpplexer():
     import re
 
     keywords = '''
-        alignas alignof and and_eq asm bitand bitor break case catch
-        class compl const_cast continue
-        default delete do double dynamic_cast else enum explicit export extern false
-        for friend goto if inline namespace new noexcept not
-        not_eq nullptr operator or or_eq private protected public
-        reinterpret_cast return sizeof static static_assert static_cast
-        struct switch template this thread_local throw true try typedef typeid typename
-        union using virtual while xor xor_eq
+        and and_eq asm bitand bitor break case catch
+        compl continue
+        default delete do  else enum explicit export extern 
+        for goto if inline namespace new noexcept not
+        not_eq operator or or_eq private protected public
+        return switch  throw try  typeid 
+        using virtual while xor xor_eq
     '''.split()
     
     context_keywords = 'final override'.split()
     
+    builtin_names = 'true false NULL nullptr this'.split()
+
+    # not all of these are types, but I was tired of seeing so much green.
     cpp_types = '''
+    union 
+    template thread_local static  static_assert static_cast alignas alignof const_cast reinterpret_cast
+    sizeof typename double dynamic_cast typedef class struct typename friend
     int long bool auto char16_t char32_t char const register mutable void volatile wchar_t
     float short signed unsigned decltype size_t ssize_t constexpr
     '''.split()
@@ -32,6 +37,7 @@ def cpplexer():
     cpp_types += ['{}int{}_t'.format(u, 2**n) for u in ('u', '') for n in range(3, 7)]
 
     CONTEXT_KW  = dict(lexcat='function')    
+    NAME        = dict(lexcat='function')
     KEYWORD     = dict(lexcat='keyword')
     ESCAPE      = dict(lexcat='escape')
     STRING      = dict(lexcat='literal')
@@ -54,9 +60,14 @@ def cpplexer():
 
     Keyword = keyword(keywords, KEYWORD) 
     CtxKeywords = keyword(context_keywords, CONTEXT_KW)
+    BuiltinNames = keyword(builtin_names, NAME)
     Type    = keyword(cpp_types, TYPE)
 
-    
+
+    QualName = regex(r'\w\s*(::|\.|->)\s*(?P<body>\w+)(?!\s*(::|\.|->))', NAME)
+
+
+
     Esc1        = regex(r'''\\[abfnrtv'"\\]''', ESCAPE)
     Esc2        = regex(r'''\\\[0-7]{1,3}''', ESCAPE)
     Esc3        = regex(r'''\\x[a-fA-F0-9]{2}''', ESCAPE)
@@ -110,7 +121,7 @@ def cpplexer():
     CPP = region(
             guard=None,
             exit=None,
-            contains=[DQString, Keyword, CPPComment, 
+            contains=[DQString, Keyword, CPPComment, BuiltinNames, QualName,
                       CComment, DoxyCPPComment, DoxyCComment,
                       Preproc, Type, HexLit, DecLit, FloatLit, CharLit,
                       CtxKeywords]
@@ -119,15 +130,16 @@ def cpplexer():
     Preproc.contains += CPP.contains
     return CPP
 
-
-@autoconnect(BufferController.buffer_needs_highlight,
-             lambda tags: tags.get('syntax') == 'c++')
-def cpp_syntax_highlghting(bufctl):
-    highlighter = SyntaxHighlighter(
-        'stem.plugins.cpp.syntax',
-        cpplexer(),
-        dict(lexcat=None)
-    )
-
-    highlighter.highlight_buffer(bufctl.buffer)
-
+# 
+# @autoconnect(BufferController.buffer_needs_highlight,
+#              lambda tags: tags.get('syntax') == 'c++')
+# def cpp_syntax_highlghting(bufctl):
+#     highlighter = SyntaxHighlighter(
+#         'stem.plugins.cpp.syntax',
+#         cpplexer(),
+#         dict(lexcat=None)
+#     )
+# 
+#     highlighter.highlight_buffer(bufctl.buffer)
+# 
+# 
