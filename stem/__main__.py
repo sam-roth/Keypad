e39cc1d3974b7ef0d58534fef4eabf655a23a21c
Sam Roth <sam.roth1@gmail.com>
2014-06-14 12:09:47 -0500

    Clean up the __main__ module and disable a SIP feature that occasionally causes SIGSEGVs.

--- stem/__main__.py
+++ stem/__main__.py
@@ -1,23 +1,33 @@
 
 
-
-def main():
-    import sys
+def _setup_logging():
     import logging
     logfmt = '[%(asctime)s|%(module)s:%(lineno)d|%(levelname)s]\n  %(message)s'
 
     logging.basicConfig(level=logging.DEBUG,
                         format=logfmt)
-    import os.path
-    thirdparty = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'third-party'))
-    import sys
-    sys.path.insert(0, thirdparty)
+
+def main():
+    _setup_logging() # needs to be done before using the logger
+
+    import sys, sip, os.path
     from . import config, options
-#     import importlib
-#     importlib.import_module(options.DefaultDriverMod).main()
     from .qt import driver
 
-    # allow for sending a commandline command at startup
+
+    # As for memory, modern OSes handle that just fine, so we don't need to free
+    # memory on exiting. The GC does this in a non-deterministic order, which can
+    # cause SEGVs. Other resources may be left open by this, but it's generally
+    # not okay to rely on GC for those anyway, so we shouldn't be any worse off.
+    sip.setdestroyonexit(False) 
+
+    # Add the `third-party` dir into the path.
+    thirdparty = os.path.abspath(os.path.join(os.path.dirname(__file__), 
+                                              os.path.pardir,
+                                              'third-party'))
+    sys.path.insert(0, thirdparty)
+
+    # Allow for sending a commandline command at startup.
     if len(sys.argv) > 2 and sys.argv[1] == '-c':
         from .core.notification_queue import run_in_main_thread
         from .control.command_line_interpreter import CommandLineInterpreter
@@ -27,7 +37,7 @@ def main():
         interp = CommandLineInterpreter()
 
         def win_creation_hook(win):
-            # responder chain may not be updated yet
+            # Responder chain may not be updated yet.
             try:
                 interp.exec(app(), sys.argv[2])
             except NoSuchCommandError:
